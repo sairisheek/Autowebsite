3500aeb5c2d81e232300311d72d47dcec008fa41
Sairisheek Muttukuru <sairisheekm@gmail.com>
2016-05-04 22:21:31 -0700

    pg test

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 var express = require('express');
 var app = express();
+var pg = require('pg');
+
 
 var port = process.env.PORT || 8080;
+
+console.log(process.env.DATABASE_URL);
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+	client.query('SELECT * FROM master_table', function(err, result) {
+	   done();
+	   if(err) return console.error(err);
+	   console.log(result.rows);
+	   });
+	
+
+
+	});
+
+
 
 app.get('/', function(req, res){
 	res.send('Hello world!');
