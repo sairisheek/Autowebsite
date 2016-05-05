6181638e7608f08ff266cce84892c5afd39cced6
Sairisheek Muttukuru <sairisheekm@gmail.com>
2016-05-05 01:48:51 -0700

    testing router

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 var express = require('express');
 var app = express();
 var pg = require('pg');
+var router = express.Router();
 
-var text;
+
 var port = process.env.PORT || 8080;
 
 console.log("Here is  dburl: "+process.env.DATABASE_URL);
-pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-	client.query('SELECT * FROM master_table', function(err, result) {
-	   done();
-	   if(err) return console.error(err);
-		text = result.rows;
-	   });
-	
+var conString = process.env.DATABASE_URL;
 
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
 
-	});
-
-
+router.get("/login",function(req,res){
+  res.sendFile("/resources/index.html");
+});
 
 app.get('/', function(req, res){
+	var client = new pg.Client(conString);
+	client.on('drain', client.end.bind(client));
+	client.connect();
+	var query = client.query("SELECT * FROM master_table");
+	query.on('row', function(row){
+		var text = row;
+	})
+
+
 	res.send(text);
 });
 
