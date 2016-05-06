77ac984a3effcd0e16ffd86895508f2c83cb14bf
Sairisheek Muttukuru <sairisheekm@gmail.com>
2016-05-05 19:46:00 -0700

    testing post

--- index.js
+++ index.js
@@ -24,8 +24,6 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
-app.use(express.json());       // to support JSON-encoded bodies
-app.use(express.urlencoded()); // to support URL-encoded bodies
 
 //routing for GET /login
 app.get("/login",function(req,res){
