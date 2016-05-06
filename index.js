97ce73f67f5ed5346e1e6d8e6bcd970feea0fa4c
Sairisheek Muttukuru <sairisheekm@gmail.com>
2016-05-06 01:46:54 -0700

    pushing authentication algorithm

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 var router = express.Router();
+var passport = require('passport');
+var flash    = require('connect-flash');
 
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+
+var session      = require('express-session')
 var bodyParser = require('body-parser');
 var text;
 
@@ -19,6 +25,19 @@ var conString = process.env.DATABASE_URL;
 });
 */
 
+require('passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+// required for passport
+app.use(session({ secret: 'badbitchcontestyouinfirstplace' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -27,20 +46,28 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 //routing for GET /login
 app.get("/login",function(req,res){
-  res.sendFile(process.env.PWD+"/resources/index.html");
+  res.render(process.env.PWD+"/resources/index.html",{ message: req.flash('loginMessage') }
 }); 
 
 //routing for POST /login
-app.post("/login",function(req,res){
-	console.log(req.body.inputEmail);
-	console.log(req.body.inputPassword);
-});
+
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/home', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
 
 //app.use("/",router);
 // loading static files
 app.use("/css", express.static(__dirname + '/resources/css'));
 app.use("/fonts", express.static(__dirname + '/resources/fonts'));
 app.use("/js", express.static(__dirname + '/resources/js'));
+
+//routing for /home
+app.get('/home',isLoggedin, function(req,res){
+	res.sendFile(--__dirname+'/resources/search.html');
+})
+
 
 // routing for root
 app.get('/', function(req, res){
@@ -55,6 +82,31 @@ app.get('/', function(req, res){
 
 	res.send(text);
 });
+
+
+// =====================================
+    // LOGOUT ==============================
+    // =====================================
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+};
+
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated()) {
+        console.log('isLoggedin');
+        return next();
+    }
+    console.log('is not logged in');
+
+    // if they aren't redirect them to the home page
+    res.redirect('/login');
+}
 
 app.listen(port, function(){
 	console.log('Running! '+port);
---
+++ users.js
@@ -0,0 +1,82 @@
+var pg           = require('pg');
+
+
+
+var client = new pg.Client(process.env.DATABASE_URL);
+
+
+
+function User(){
+    this.c_id = 0;
+    this.username = "";
+    this.password= ""; //need to declare the things that i want to be remembered for each user in the database
+
+      
+}
+
+User.findOne = function(username, callback){
+
+    var client = new pg.Client(process.env.DATABASE_URL);
+
+    var isNotAvailable = false; //we are assuming the username is taking
+    //var username = this.username;
+    //var rowresult = false;
+    console.log(username + ' is in the findOne function test');
+    //check if there is a user available for this username;
+    client.connect();
+   
+
+    client.query("SELECT * from authentication_table where username=$1", [username], function(err, result){
+        if(err){
+            return callback(err, isNotAvailable, this);
+        }
+        //if no rows were returned from query, then new user
+        if (result.rows.length > 0){
+            isNotAvailable = true; // update the user for return in callback
+            ///username = username;
+            //password = result.rows[0].password;
+            console.log(username + ' is am not available!');
+        }
+        else{
+            isNotAvailable = false;
+            //username = username;
+            console.log(username + ' is available');
+        }
+        //the callback has 3 parameters:
+        // parameter err: false if there is no error
+        //parameter isNotAvailable: whether the username is available or not
+        // parameter this: the User object;
+
+        client.end();
+        return callback(false, isNotAvailable, this);
+
+
+    });
+
+User.findById = function(id, callback){
+    console.log("we are in findbyid");
+ 
+    var client = new pg.Client(process.env.DATABASE_URL);
+
+    client.connect();
+    client.query("SELECT * from authentication_table where customer_id=$1", [id], function(err, result){
+
+        if(err){
+            return callback(err, null);
+        }
+        //if no rows were returned from query, then new user
+        if (result.rows.length > 0){
+            console.log(result.rows[0] + ' is found!');
+            var user = new User();
+            user.username= result.rows[0]['username'];
+            user.password = result.rows[0]['password'];
+            user.c_id = result.rows[0]['customer_id'];
+            console.log(user.email);
+            return callback(null, user);
+        }
+    });
+};
+
+
+
+module.exports = User;
