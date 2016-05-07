dc028e1ae72c82459467e3b5502364b94edcb411
Sairisheek Muttukuru <sairisheekm@gmail.com>
2016-05-07 15:57:44 -0700

    cleanup

--- index.js
+++ index.js
@@ -3,14 +3,7 @@
 var express = require('express');
 var app = express();
 var pg = require('pg');
-var router = express.Router();
-var passport = require('passport');
-var flash    = require('connect-flash');
 
-var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
-
-var session      = require('express-session')
 var bodyParser = require('body-parser');
 var text;
 
@@ -19,46 +12,21 @@ console.log(process.env.PWD);
 console.log("Here is  dburl: "+process.env.DATABASE_URL);
 var conString = process.env.DATABASE_URL;
 
-/*router.use(function (req,res,next) {
-  console.log("/" + req.method);
-  next();
-});
-*/
 
- // pass passport for configuration
 
-// set up our express application
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
-
-// required for passport
-app.use(session({ secret: 'badbitchcontestyouinfirstplace' })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
-
-require(__dirname+'/passport')(passport);
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
-app.use(express.static(__dirname + '/resources'));
+
 
 //routing for GET /login
 app.get("/login",function(req,res) {
   res.sendFile( process.env.PWD+"/resources/index.html" )
 }); 
 
-//routing for POST /login
 
-    app.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/home', // redirect to the secure profile section
-        failureRedirect : '/login', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
-    }));
 
-//app.use("/",router);
 // loading static files
 app.use("/css", express.static(__dirname + '/resources/css'));
 app.use("/fonts", express.static(__dirname + '/resources/fonts'));
@@ -83,29 +51,6 @@ app.get('/', function(req, res){
 });
 
 
-// =====================================
-    // LOGOUT ==============================
-    // =====================================
-    app.get('/logout', function(req, res) {
-        req.logout();
-        res.redirect('/');
-    });
-
-
-
-// route middleware to make sure a user is logged in
-function isLoggedIn(req, res, next) {
-
-    // if user is authenticated in the session, carry on
-    if (req.isAuthenticated()) {
-        console.log('isLoggedin');
-        return next();
-    }
-    console.log('is not logged in');
-
-    // if they aren't redirect them to the home page
-    res.redirect('/login');
-}
 
 app.listen(port, function(){
 	console.log('Running! '+port);
