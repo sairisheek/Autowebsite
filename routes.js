e8b7120406c78c1ec0a0abc1f3efb3f0338ad625
Sairisheek Muttukuru <sairisheekm@gmail.com>
2016-05-07 18:41:52 -0700

    passport auth debug

--- routes.js
+++ routes.js
@@ -15,6 +15,7 @@ app.get('/home',isLoggedIn, function(req,res){
 	res.sendFile(__dirname+'/resources/search.html');
 });
 
+//routing for auth
 app.post('/login', passport.authenticate('local-login', {
 			successRedirect : '/home', // redirect to the secure profile section
 			failureRedirect : '/login', // redirect back to the signup page if there is an error
@@ -24,15 +25,15 @@ app.post('/login', passport.authenticate('local-login', {
 
 // routing for root
 app.get('/', function(req, res){
-	var client = new pg.Client(process.env.DATABASE_URL);
-	client.on('drain', client.end.bind(client)); //after queries have completed, close the pool
-	client.connect();
-	var query = client.query("SELECT * FROM master_table");
-	query.on('row', function(row){
-		 text = row;
-	});
-	res.send(text);
+	res.render('index.ejs',{ message: req.flash('loginMessage') })
 });
+
+//routing for logout
+app.get('/logout',function(req,res){
+	res.logout();
+	res.redirect('/');
+}); 
+
 
 };
 
