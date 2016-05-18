8a2eb09d708176c2c78826310414ba9b7ef40d9a
Sairisheek Muttukuru <sairisheekm@gmail.com>
2016-05-18 04:09:10 -0700

    adding add

--- pglinks.js
+++ pglinks.js
@@ -1,5 +1,5 @@
 exports.createCustomer = function(sequelize,newUser){ 
-sequlize.sync().success(function (err){
+sequelize.sync().success(function (err){
 	var Customer = sequelize.import('./models/customer.js');
 	Customer.create(newUser).success(function () {
         console.log('Customer: '+ newUser);
